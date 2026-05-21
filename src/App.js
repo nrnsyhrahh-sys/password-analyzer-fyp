c4817a50aab65bfda460bffee26c90e8b719a264
Nurin <nrnsyhrahh@gmail.com>
2026-05-22 02:39:05 +0800

    update backend URL

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function PasswordAnalyzer() {
     if (!password) return;
     setLoading(true);
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/analyze', {
+      const response = await fetch('http://https://password-analyzer-fyp.onrender.com/api/analyze', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ password: password })
